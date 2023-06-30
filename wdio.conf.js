d41b70fea07c1773e6262f979fccc152e082c5be
Murat Karaiev <qa.froggy@gmail.com>
2023-06-30 12:51:15 +0300

    created specs folders

--- wdio.conf.js
+++ wdio.conf.js
@@ -18,7 +18,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/regression/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -52,7 +52,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'firefox'
+        browserName: 'chrome'
     }],
     //
     // ===================
@@ -74,7 +74,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'https://www.booking.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
