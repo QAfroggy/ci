f5545014305d3e8e14d638605d47773fdcfdc5d6
Murat Karaiev <qa.froggy@gmail.com>
2023-06-30 15:18:13 +0300

    444

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,6 +5,7 @@ exports.config = {
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
+    headless: true,
     
     //
     // ==================
