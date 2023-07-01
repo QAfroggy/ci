b7b09d7b3c923febb4afecdf3168b179e3e7dbcd
Murat Karaiev <qa.froggy@gmail.com>
2023-07-01 13:03:00 +0300

    timeout 60000

--- wdio.conf.js
+++ wdio.conf.js
@@ -104,7 +104,13 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services:[
+        ['chromedriver', {
+          // other configuration options
+          waitforTimeout: 60000 // Increase the timeout value to 60 seconds (or more if needed)
+        }]
+      ],
+
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
