eb4c4b873a86919e50f6d0e4602434a0ab92b9dd
Murat Karaiev <qa.froggy@gmail.com>
2023-07-01 20:29:41 +0300

    hej

--- wdio.conf.js
+++ wdio.conf.js
@@ -104,11 +104,9 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services:[
-        ['chromedriver', {
-          // other configuration options
-          waitforTimeout: 60000 // Increase the timeout value to 60 seconds (or more if needed)
-        }]
+    services:
+        ['chromedriver'
+       
       ],
 
     
