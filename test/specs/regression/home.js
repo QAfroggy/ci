6225254f4ec7fe2b9c4ed93beb3b25d523ae03c8
Murat Karaiev <qa.froggy@gmail.com>
2023-07-01 11:41:51 +0300

    navig ul

--- test/specs/regression/home.js
+++ test/specs/regression/home.js
@@ -10,6 +10,27 @@ it("Home page is visible", async()=>{
     const page = await $(Home.selectors.homePage);
     await expect(page).toBePresent();
 });
+it("Logo  is visible", async()=>{
+    const page = await $(Home.selectors.logo);
+    await expect(page).toBePresent();
+});
+it("Logo  has aria-label 'Booking.com'", async()=>{
+    const page = await $(Home.selectors.logo);
+    await expect(page).toHaveAttribute('aria-label', 'Booking.com');
+});
+
+
+it("Navigation is visible", async()=>{
+    const page = await $(Home.selectors.navigation);
+    await expect(page).toBePresent();
+});
+it("Navigation has ul ", async()=>{
+    const list = await $$(Home.selectors.listItemsNavigation);
+    console.log("=======================")
+    console.log("----------> ", + list.length)
+   await expect(list).toHaveChildren({ gte: 1 });
+});
+
 
 
 });
--- utils/pages/home.js
+++ utils/pages/home.js
@@ -2,6 +2,9 @@
 
 const selectors = {
 homePage: '#b2indexPage',
+logo: '[data-testid="header-booking-logo"]',
+navigation: '[data-testid="header-xpb"]',
+listItemsNavigation: '[data-testid="header-xpb"] div ul li '
 }
 
 
