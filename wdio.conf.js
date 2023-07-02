c6ecb3df1f5432949b2892c5a7d65c5eb02dc399
Murat Karaiev <qa.froggy@gmail.com>
2023-07-02 19:12:44 +0300

    fix

--- wdio.conf.js
+++ wdio.conf.js
@@ -104,10 +104,17 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services:
-        ['chromedriver'
-       
+
+    outputDir: 'all-logs',
+    services: [
+        ['chromedriver', {
+            logFileName: 'wdio-chromedriver.log', // default
+            outputDir: 'driver-logs', // overwrites the config.outputDir
+            args: ['--silent']
+        }]
       ],
+      // ...
+    
 
     
     // Framework you want to run your specs with.
