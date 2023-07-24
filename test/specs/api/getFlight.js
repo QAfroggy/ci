b5a0ec508bb5a81795fc495ce485fbc9260a94ce
Murat Karaiev <qa.froggy@gmail.com>
2023-07-24 14:09:45 +0300

    del api

--- test/specs/api/getFlight.js
+++ test/specs/api/getFlight.js
@@ -1,15 +1,1 @@
-import axios from "axios";
-
-describe('axios', ()=>{
-  
-it("Get fligths", async()=>{
-    try{
-const response = await axios.get("https://www.booking.com/attractions/api/header?label=gog235jc-1FCAEoggI46AdIIVgDaLYBiAEBmAEhuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKDguGlBsACAdICJGU1Y2EwZjJiLTY2ZjctNGI2OC05YjE4LWM3MzAwYWE3MTJhZdgCBeACAQ&lang=en-us&aid=397594&product=flights",)
-console.log("--->>>", + response)
-//await expect(response.statusText).to.eq("OK")
-    } catch(error){
-        expect(error.response.status).to.eq(NaN)
-    }
-
-});
-    });
+// q
