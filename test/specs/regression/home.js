849628b3323376bb6a3bf64639f36efbb69a9da1
Murat Karaiev <qa.froggy@gmail.com>
2023-07-24 15:11:06 +0300

    comment script

--- test/specs/regression/home.js
+++ test/specs/regression/home.js
@@ -1,37 +1,38 @@
-import Home from '../../../utils/pages/home.js'
+// import Home from '../../../utils/pages/home.js'
 
-describe("Elements", async()=>{
+// describe("Elements", async()=>{
 
-before(async()=>{
-    Home.openPage();
-});
+// before(async()=>{
+//     Home.openPage();
+// });
 
-it("Home page is visible", async()=>{
-    const page = await $(Home.selectors.homePage);
-    await expect(page).toBePresent();
-});
-it("Logo  is visible", async()=>{
-    const page = await $(Home.selectors.logo);
-    await expect(page).toBePresent();
-});
-it("Logo  has aria-label 'Booking.com'", async()=>{
-    const page = await $(Home.selectors.logo);
-    await expect(page).toHaveAttribute('aria-label', 'Booking.com');
-});
-
-
-it("Navigation is visible", async()=>{
-    const page = await $(Home.selectors.navigation);
-    await expect(page).toBePresent();
-});
-it("Navigation has ul ", async()=>{
-    const list = await $$(Home.selectors.listItemsNavigation);
-    console.log("=======================")
-    console.log("----------> ", + list.length)
-   await expect(list).toHaveChildren({ gte: 1 });
-});
-//test
+// it("Home page is visible", async()=>{
+//     const page = await $(Home.selectors.homePage);
+//     await expect(page).toBePresent();
+// });
+// it("Logo  is visible", async()=>{
+//     const page = await $(Home.selectors.logo);
+//     await expect(page).toBePresent();
+// });
+// it("Logo  has aria-label 'Booking.com'", async()=>{
+//     const page = await $(Home.selectors.logo);
+//     await expect(page).toHaveAttribute('aria-label', 'Booking.com');
+// });
 
 
+// it("Navigation is visible", async()=>{
+//     const page = await $(Home.selectors.navigation);
+//     await expect(page).toBePresent();
+// });
+// it("Navigation has ul ", async()=>{
+//     const list = await $$(Home.selectors.listItemsNavigation);
+//     console.log("=======================")
+//     console.log("----------> ", + list.length)
+//    await expect(list).toHaveChildren({ gte: 1 });
+// });
+// //test
 
-});
+
+
+// });
+console.log("hello")
